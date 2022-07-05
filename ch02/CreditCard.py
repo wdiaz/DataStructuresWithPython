d3592480cf5fc01d7b2cac61bc8c63cb3ad7071d
Walter Diaz <undisclosed@undisclosed.com>
2022-07-04 20:39:32 -0400

    added predaroty credit card. Inherited

--- ch02/CreditCard.py
+++ ch02/CreditCard.py
@@ -35,3 +35,44 @@ class CreditCard:
 
     def make_payment(self, amount):
         self._balance -= amount
+
+
+class PredatoryCreditCard(CreditCard):
+    def __init__(self, customer, bank, account, limit, apr):
+        """Create a new predatory credit card instance"""
+        super().__init__(customer, bank, account, limit)
+        self._apr = apr
+
+    def charge(self, price):
+        success = super().charge(price)
+        if not success:
+            self._balance += 5  # use mutators instead
+        return success
+
+    def process_month(self):
+        if self.get_balance() > 0:
+            monthly_factor = pow(1 + self._apr, 1/2)
+            self._balance *= monthly_factor
+
+
+if __name__ == '__main__':
+    wallet = [CreditCard('John Thomas', 'California Savings', '5391 0375 9387 5309', 2500),
+              CreditCard('John Thomas', 'California Federal', '5391 0375 4532 5223', 3500),
+              CreditCard('John Thomas', 'California Finance', '5391 3233 3266 1355', 5000)]
+
+    for val in range(1, 17):
+        wallet[0].charge(val)
+        wallet[1].charge(2 * val)
+        wallet[2].charge(3 * val)
+
+    for c in range(3):
+        print(f'Customer = {wallet[c].get_customer()}')
+        print(f'Bank= {wallet[c].get_bank()}')
+        print(f'Account= {wallet[c].get_account()}')
+        print(f'Limit= {wallet[c].get_limit()}')
+        print(f'Balance= {wallet[c].get_balance()}')
+
+        while wallet[c].get_balance() > 100:
+            wallet[c].make_payment(100)
+            print(f'New Balance: {wallet[c].get_balance()}')
+        print()
